b4bd55a3ac9e1dff51e567d3af3db87d5e01941b
edisonalvarezv10 <edisonalvarezv10@gmail.com>
2025-12-12 05:11:32 -0500

    config con la api del backend

--- src/frontend/globales/config.js
+++ src/frontend/globales/config.js
@@ -1,4 +1,4 @@
 // Detecta si estás en local o en producción automáticamente
 const API_BASE_URL = window.location.hostname.includes('localhost') || window.location.hostname.includes('127.0.0.1')
     ? 'http://127.0.0.1:8000' // Tu URL local de Django
-    : 'https://gestion-academica-del-instituto.onrender.com'; // La URL que Render te dará para el backend
+    : 'https://backend-instituto-educativo-infancia.onrender.com'; // La URL que Render te dará para el backend
